f4b40259269856894646dc976d160c827a8097f7
Antonytm <antontishchenko@gmail.com>
2018-07-19 00:12:19 +0300

    Change name of configuration file

--- build/build.ps1
+++ build/build.ps1
@@ -13,7 +13,7 @@ New-Item -Name build\package\bin -ItemType directory
 New-Item -Name build\package\App_Config\Include\Foundation -ItemType directory
 
 Copy-Item .\src\Foundation\CSSGridLayout\code\bin\Foundation.CSSGrid* .\build\package\bin
-Copy-Item .\src\Foundation\CSSGridLayout\code\App_Config\Include\CSSGridLayout\Foundation.Sitecore.CSSGridLayout.config .\build\package\App_Config\Include\Foundation
+Copy-Item .\src\Foundation\CSSGridLayout\code\App_Config\Include\CSSGridLayout\Foundation.CSSGridLayout.config .\build\package\App_Config\Include\Foundation
 Copy-Item .\src\Foundation\CSSGridLayout\serialization\* .\build\package\Data -recurse
 
 $packageCmd = "Sitecore.Courier.Runner.exe -t build\package -o build\artifacts\sitecore.cssgrid." + $version + ".update -r"
